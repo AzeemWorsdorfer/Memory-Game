adb8817806da5ba0cc5c18d45e75273a17cb530f
AzeemWorsdorfer <mohazeem327@gmail.com>
2026-01-29 09:02:06 +0100

    chore: uppdate format of Win message.

--- src/components/WinMessage.jsx
+++ src/components/WinMessage.jsx
@@ -2,7 +2,7 @@ export const WinMessage = ({ moves }) => {
     return (
         <div className="win-message">
         <h2>Congratulations!</h2>
-        <p>You completed the game in { moves }moves!</p>
+        <p>You completed the game in { moves } moves!</p>
         </div>
     );
 }
